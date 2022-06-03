5ce0e0855dc04a83f19b8508fb6e8e666cb5c69f
Geya Sahithi <73871382+Geya-Sahithi@users.noreply.github.com>
2022-06-03 22:05:43 +0530

    Delete MaxMin.java

--- Java Basics II/MaxMin.java
+++
@@ -1,7 +0,0 @@
-//Write a Java program to find the maximum and minimum value of an array
-// public class MaxMin {
-//     public static void main(String[] args) {
-//         Integer arr[] = new Integer[]{1,2,3,4,5,6};
-        
-//     }
-// }
