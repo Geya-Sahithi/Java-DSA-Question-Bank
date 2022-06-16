e2f49f5dc51e22fe3a053e9799e06b9e811b215a
Geya Sahithi <73871382+Geya-Sahithi@users.noreply.github.com>
2022-06-16 12:12:42 +0530

    added file

---
+++ Array and Matrix/ArrayDist.java
@@ -0,0 +1,15 @@
+// import java.time.chrono.MinguoChronology;
+
+// public class ArrayDist {
+//     static int maxValueOfJMinusI(int a[]){
+//         int n = a.length;
+//         int rightMas = new int[n];
+//         int leftMax = new int[n];
+
+//         int ans;
+//         return ans;
+//     }
+//     public static void main(String[] args) {
+        
+//     }
+// }
