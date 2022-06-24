4e706372e1a31ae98cd6b8bbbfa6cdac483aa7a2
Geya Sahithi <73871382+Geya-Sahithi@users.noreply.github.com>
2022-06-24 21:49:24 +0530

    Add files via upload

---
+++ Mathematics basics/GcdOrHcf.java
@@ -0,0 +1,14 @@
+public class GcdOrHcf {
+    
+    static int gcd(int a,int b) {
+        System.out.println(a + " " + b);
+        if(a == b) return a;
+        if(a < b) return gcd(b, a);
+        return gcd(a-b, b);
+    }
+    public static void main(String[] args) {
+        int a = 60;
+        int b = 36;
+        System.out.println(gcd(a, b));
+    }
+}
