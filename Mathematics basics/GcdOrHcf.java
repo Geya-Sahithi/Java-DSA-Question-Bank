243285e7cadcf704cf640d0c55afe3139e32486c
Geya Sahithi <73871382+Geya-Sahithi@users.noreply.github.com>
2022-06-26 11:17:32 +0530

    Update GcdOrHcf.java

--- Mathematics basics/GcdOrHcf.java
+++ Mathematics basics/GcdOrHcf.java
@@ -1,11 +1,18 @@
 public class GcdOrHcf {
     
-    static int gcd(int a,int b) {
-        System.out.println(a + " " + b);
+    static int gcd(int a, int b) {
         if(a == b) return a;
-        if(a < b) return gcd(b, a);
+        if(a < b) return gcd(b,a);
         return gcd(a-b, b);
+
     }
+    
+//     static int gcd(int a,int b) {
+//         System.out.println(a + " " + b);
+//         if(a == b) return a;
+//         if(a < b) return gcd(b, a);
+//         return gcd(a-b, b);
+//     }
     public static void main(String[] args) {
         int a = 60;
         int b = 36;
