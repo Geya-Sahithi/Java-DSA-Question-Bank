51cfbdfd30ff6b31566247bafac545ff8bdaceb6
Geya Sahithi <73871382+Geya-Sahithi@users.noreply.github.com>
2022-07-05 12:53:52 +0530

    Update SetsInBit.java

--- Bit/SetsInBit.java
+++ Bit/SetsInBit.java
@@ -10,7 +10,21 @@ public class SetsInBit {
         }
         return count;
     }    
+    
+    static int countSetBitsOptimised(int n) {
+        int count = 0;
+
+        while(n > 0) {
+            count ++;
+            n = n & (n-1);
+        }
+        return count;
+    }
+    
     public static void main(String[] args) {
         System.out.println(countSetBits(15));
+        
+        System.out.println(countSetBitsOptimised(15));
+
     }
 }
