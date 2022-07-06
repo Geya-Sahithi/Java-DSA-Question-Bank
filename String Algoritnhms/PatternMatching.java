9c527e51eab947a4e8414f21c6913dba5bbf10af
Geya Sahithi <73871382+Geya-Sahithi@users.noreply.github.com>
2022-07-06 21:59:51 +0530

    Add files via upload

---
+++ String Algoritnhms/PatternMatching.java
@@ -0,0 +1,26 @@
+public class PatternMatching {
+    
+    static void patternMatching(String s,String p) {
+        int n = s.length();
+        int m = p.length();
+
+        for (int i = 0; i <= n-m ; i++) {
+            int j =0;
+            for (; j < m; j++) {
+                if(s.charAt(i+j) != p.charAt(j)){
+                    break;
+                }
+            }
+            if(j == m) {
+                System.out.println("pattern found at " + i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabdabbacdab";
+        String p = "bda";
+
+        patternMatching(s, p);
+    }
+}
